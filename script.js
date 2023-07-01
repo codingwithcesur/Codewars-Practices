ac5856f37409243db3494a83b52a074b6fedd519
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-07-01 16:18:24 +0300

    function with two arguments that will return an array of the first n multiples of x

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function reverseStr(str) {
 function numberToString(num) {
   return num.toString();
 }
+
+// function with two arguments that will return an array of the first n multiples of x.Assume both the given number and the number of times to count will be positive numbers greater than 0.
+function countBy(x, n) {
+  var z = [];
+  for (i = 1; i <= n; i++) {
+    z.push(x * i);
+  }
+  return z;
+}

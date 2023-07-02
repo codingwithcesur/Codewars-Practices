9b745259c351903905dbed2bdb825770ae1d11b5
Furkan Cesur <86592976+codingwithcesur@users.noreply.github.com>
2023-07-02 13:29:10 +0300

    add Summation of every number from 1 to num

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function numberToString(num) {
 
 // function with two arguments that will return an array of the first n multiples of x.Assume both the given number and the number of times to count will be positive numbers greater than 0.
 function countBy(x, n) {
-  var z = [];
+  let z = [];
   for (i = 1; i <= n; i++) {
     z.push(x * i);
   }
@@ -21,3 +21,12 @@ function countBy(x, n) {
 function century(year) {
   return Math.ceil(year / 100);
 }
+// Summation of every number from 1 to num. Assuming the number will always be a positive integer greater than 0.
+
+var summation = function (num) {
+  let result = 0;
+  for (let i = 1; i <= num; i++) {
+    result += i;
+  }
+  return result;
+};
